2d8e5fe2f2339b38ea85089a244d62e28c1bda65
Ujjwal Kumar Shukla <100704162+ujjwalkshukla@users.noreply.github.com>
2026-03-07 14:00:42 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import path from "path";
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, ".", "");
@@ -15,8 +14,7 @@ export default defineConfig(({ mode }) => {
     },
 
     plugins: [
-      react(),
-      tailwindcss()
+      react()
     ],
 
     define: {
